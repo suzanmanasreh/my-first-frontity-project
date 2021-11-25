d3bacc290576317922977e853e5c8eed5af6438b
Suzan M <suzanmanasreh@gmail.com>
2021-11-25 15:56:58 -0500

    change header

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,57 +1,46 @@
 const settings = {
-  "name": "my-first-frontity-project",
-  "state": {
-    "frontity": {
-      "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
-    }
+  name: "my-first-frontity-project",
+  state: {
+    frontity: {
+      url: "https://test.frontity.org",
+      title: "KSU Bioinformatics Lab",
+      description: "AI/ML Lab @ Kennesaw State University",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/twentytwenty-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Nature",
-              "/category/nature/"
-            ],
-            [
-              "Travel",
-              "/category/travel/"
-            ],
-            [
-              "Japan",
-              "/tag/japan/"
-            ],
-            [
-              "About Us",
-              "/about-us/"
-            ]
+      name: "@frontity/twentytwenty-theme",
+      state: {
+        theme: {
+          menu: [
+            ["News", "/news"],
+            ["Research", "/research/"],
+            ["People", "/people/"],
+            ["Publications", "/publications/"],
+            ["Supplements", "/supplements/"],
+            ["Software", "/software/"],
+            ["Positions", "/positions/"],
+            ["Galleries", "/galleries/"],
+            ["Contact", "/contact/"],
           ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "url": "https://test.frontity.org"
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://test.frontity.org",
+        },
+      },
     },
     "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    "@frontity/html2react",
+  ],
 };
 
 export default settings;
